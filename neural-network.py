ce73f316ad0167ef6440ac0168419daf9eaf5c34
tejas-sri <38899432+tejas-sri@users.noreply.github.com>
2018-12-23 21:00:46 -0500

    Update neural-network.py

--- neural-network.py
+++ neural-network.py
@@ -1,21 +1,59 @@
 import numpy as np
+import math
 
-def nonlin(x,deriv=False):
-    if(deriv==True):
-        return x*(1-x)
-    return 1/(1+np.exp(-x))
+sin=np.vectorize(math.sin)
+cos=np.vectorize(math.cos)
+weight1=np.array([])
+weight2=np.array([])
 
-X = np.array([[0,0,1],[0,1,1], [1,0,1],[1,1,1]])
+class NN:
+    def __init__(self, x, y):
+        global weight1, weight2
 
-y = np.array([[0,0,1,1]]).T
+        self.x=x
+        self.y=y
+        self.output=np.zeros(y.shape)
+        
+        self.weights1=np.random.rand(self.x.shape[1], 4)
+        weight1=self.weights1[:]
+        
+        self.weights2=np.random.rand(4, 1)
+        weight2=self.weights2[:]
 
-syn0 = 2*np.random.random((3,1)) - 1
+    def sigmoid(x, deriv=False):
+        if deriv:
+            return x*(1-x)
+        return np.exp(x)/(1+np.exp(x))
+    
+    def feedforward(self):
+        global weight1, weight2
 
-for i in range(10000):
-    l0 = X
-    l1 = nonlin(np.dot(l0,syn0))
-    l1_error = y - l1
-    l1_delta = l1_error * nonlin(l1,True)
-    syn0 += np.dot(l0.T,l1_delta)
+        self.layer1=NN.sigmoid(np.dot(self.x, weight1))
+        self.output=NN.sigmoid(np.dot(self.layer1, weight2))
 
-print (l1)
+    def backprop(self):
+        global weight1, weight2
+
+        d_weights2=np.dot(self.layer1.T, 2*(self.y-self.output)*NN.sigmoid(self.output, True))
+        
+        d_weights1 = np.dot(self.x.T,  (np.dot(2*(self.y - self.output)*NN.sigmoid(self.output, True), self.weights2.T)*NN.sigmoid(self.layer1, True)))
+
+        self.weights1 += d_weights1
+        weight1=self.weights1[:]
+        
+        self.weights2 += d_weights2
+        weight2=self.weights2[:]
+
+c=[[1, 1, 1], [1, 0, 1], [0, 1, 0], [0, 0, 1], [1, 1, 0]]
+d=[[1], [0], [1], [1], [0]]
+
+c=np.array(c)
+d=np.array(d)
+
+train=NN(c, d)
+
+for i in range(1500):
+    NN.feedforward(train)
+    NN.backprop(train)
+
+print(train.output)
